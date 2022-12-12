9bec06f20ce2c47c03b6c987f0abe4fe351f7d89
jzbor <jzbor@posteo.net>
2022-12-12 13:54:57 +0100

    Refactoring x11 error handling

--- libmars/src/x11.rs
+++ libmars/src/x11.rs
@@ -31,40 +31,48 @@ extern "C" fn on_wm_detected(_: *mut xlib::Display, _: *mut xlib::XErrorEvent) -
     panic!("Another Window Manager seems to be running already");
 }
 
-extern "C" fn on_error(_display: *mut xlib::Display, error: *mut xlib::XErrorEvent) -> c_int {
-    // unsafe {
-    //     let bufsize = 1024;
-    //     let mut buf = vec![0u8; bufsize];
-    //     xlib::XGetErrorText(display, (*error).error_code.into(), buf.as_mut_ptr() as *mut i8,
-    //                         (bufsize - 1) as c_int);
-    //     let msg = CString::new(buf).unwrap().into_string().unwrap();
-    //     println!("{}", msg);
-    // }
+extern "C" fn on_error(display: *mut xlib::Display, error: *mut xlib::XErrorEvent) -> c_int {
+    let msg = unsafe {
+        let bufsize = 1024;
+        let mut buf = vec![0u8; bufsize];
+        xlib::XGetErrorText(display, (*error).error_code.into(), buf.as_mut_ptr() as *mut i8,
+                            (bufsize - 1) as c_int);
+        let msg_cstring = CStr::from_ptr(buf.as_mut_ptr() as *mut i8);
+        msg_cstring.to_str().unwrap().to_owned()
+        // println!("{}", msg);
+    };
 
     unsafe {
         match (*error).error_code {
-            // @TODO add error types
-            xlib::Success => println!("everything's okay"),
-            xlib::BadRequest => panic!("bad request code"),
-            xlib::BadValue => panic!("int parameter out of range"),
-            xlib::BadWindow => println!("parameter not a Window"),
-            xlib::BadPixmap => panic!("parameter not a Pixmap"),
-            xlib::BadAtom => panic!("parameter not an Atom"),
-            xlib::BadCursor => panic!("parameter not a Cursor"),
-            xlib::BadFont => panic!("parameter not a Font"),
-            xlib::BadMatch => panic!("parameter mismatch"),
-            xlib::BadDrawable => panic!("parameter not a Pixmap or Window"),
-            xlib::BadAccess => panic!("depending on context (see X.h)"),
-            xlib::BadAlloc => panic!("insufficient resources"),
-            xlib::BadColor => panic!("no such colormap"),
-            xlib::BadGC => panic!("parameter not a GC"),
-            xlib::BadIDChoice => panic!("choice not in range or already used"),
-            xlib::BadName => panic!("font or color name doesn't exist"),
-            xlib::BadLength => panic!("Request length incorrect"),
-            xlib::BadImplementation => panic!("server is defective"),
-            _ => panic!("unknown error occured"),
+            xlib::Success => println!("X11 non-error: {}", msg),
+            _ => panic!("Fatal X11 error: {}", msg),
         }
     }
+
+    // unsafe {
+    //     match (*error).error_code {
+    //         // @TODO add error types
+    //         xlib::Success => println!("everything's okay"),
+    //         xlib::BadRequest => panic!("bad request code"),
+    //         xlib::BadValue => panic!("int parameter out of range"),
+    //         xlib::BadWindow => println!("parameter not a Window"),
+    //         xlib::BadPixmap => panic!("parameter not a Pixmap"),
+    //         xlib::BadAtom => panic!("parameter not an Atom"),
+    //         xlib::BadCursor => panic!("parameter not a Cursor"),
+    //         xlib::BadFont => panic!("parameter not a Font"),
+    //         xlib::BadMatch => panic!("parameter mismatch"),
+    //         xlib::BadDrawable => panic!("parameter not a Pixmap or Window"),
+    //         xlib::BadAccess => panic!("depending on context (see X.h)"),
+    //         xlib::BadAlloc => panic!("insufficient resources"),
+    //         xlib::BadColor => panic!("no such colormap"),
+    //         xlib::BadGC => panic!("parameter not a GC"),
+    //         xlib::BadIDChoice => panic!("choice not in range or already used"),
+    //         xlib::BadName => panic!("font or color name doesn't exist"),
+    //         xlib::BadLength => panic!("Request length incorrect"),
+    //         xlib::BadImplementation => panic!("server is defective"),
+    //         _ => panic!("unknown error occured"),
+    //     }
+    // }
 
     return 0;
 }
