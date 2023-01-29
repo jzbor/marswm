4f594075880af52120a3641d63fed09ef1f94bc9
jzbor <jzbor@posteo.net>
2023-01-29 20:01:10 +0100

    Refactoring mars-relay to use the new WMController interface

--- mars-relay/src/main.rs
+++ mars-relay/src/main.rs
@@ -1,9 +1,8 @@
+extern crate x11;
+
 use clap::Parser;
-use libmars::common::x11::atoms::*;
-use libmars::common::x11::atoms::X11Atom::*;
-use libmars::common::x11::send_client_message;
-use libmars::common::x11::window::X11Window;
-use std::ptr;
+use libmars::control::*;
+use libmars::control::x11::X11Controller;
 use x11::xlib;
 
 use crate::menu::*;
@@ -39,199 +38,95 @@ pub enum Command {
     Close,
     Menu,
     Pin,
-    Unpin,
     SendToDesktop,
     SetFullscreen,
-    ToggleFullscreen,
-    UnsetFullscreen,
     SetStatus,
     SetTiled,
-    ToggleTiled,
-    UnsetTiled,
     SwitchDesktop,
+    ToggleFullscreen,
+    ToggleTiled,
+    Unpin,
+    UnsetFullscreen,
+    UnsetTiled,
 }
-
-
-const MODE_SET: u64 = 1;
-const MODE_UNSET: u64 = 0;
-const MODE_TOGGLE: u64 = 2;
 
 
 impl Command {
-    fn execute(&self, display: *mut xlib::Display, window: xlib::Window, args: Args) -> Result<(), &'static str> {
-        match self {
-            Command::Activate => Self::simple_window_message(display, window, NetActiveWindow),
-            Command::Close => Self::simple_window_message(display, window, NetCloseWindow),
-            Command::Menu => Self::menu(display, window, args),
-            Command::Pin => Self::pin_window(display, window),
-            Command::Unpin => Self::unpin_window(display, window),
-            Command::SendToDesktop => Self::send_window_to_desktop(display, window, args.desktop),
-            Command::SetFullscreen => Self::fullscreen_window(display, window, MODE_SET),
-            Command::ToggleFullscreen => Self::fullscreen_window(display, window, MODE_TOGGLE),
-            Command::UnsetFullscreen => Self::fullscreen_window(display, window, MODE_UNSET),
-            Command::SetStatus => Self::set_status(display, args.status),
-            Command::SetTiled => Self::tile_window(display, window, MODE_SET),
-            Command::ToggleTiled => Self::tile_window(display, window, MODE_TOGGLE),
-            Command::UnsetTiled => Self::tile_window(display, window, MODE_UNSET),
-            Command::SwitchDesktop => Self::switch_desktop(display, args.desktop),
+    fn execute(&self, controller: &impl WMController<xlib::Window>, window: xlib::Window, args: Args) -> Result<(), String> {
+        if *self == Command::Menu {
+            return Self::menu(controller, window, args);
+        } else if *self == Command::SetStatus {
+            if let Some(status) = args.status {
+                return controller.set_status(status)
+                    .map_err(|e| e.to_string());
+            } else {
+                return Err("Please supply a status string".to_owned());
+            }
+        } else if *self == Command::SwitchDesktop {
+            if let Some(ws) = args.desktop {
+                return controller.switch_workspace(ws as u32)
+                    .map_err(|e| e.to_string());
+            } else {
+                return Err("Please supply a workspace".to_owned());
+            }
+        } else if *self == Command::SendToDesktop {
+            if let Some(ws) = args.desktop {
+                return controller.send_window_to_workspace(window, ws as u32)
+                    .map_err(|e| e.to_string());
+            } else {
+                return Err("Please supply a workspace".to_owned());
+            }
+        } else {
+            let result = match self {
+                Command::Activate => controller.activate_window(window),
+                Command::Close => controller.close_window(window),
+                Command::Pin => controller.pin_window(window, SettingMode::Set),
+                Command::SetFullscreen => controller.fullscreen_window(window, SettingMode::Set),
+                Command::SetTiled => controller.tile_window(window, SettingMode::Set),
+                Command::ToggleFullscreen => controller.fullscreen_window(window, SettingMode::Toggle),
+                Command::ToggleTiled => controller.tile_window(window, SettingMode::Toggle),
+                Command::Unpin => controller.pin_window(window, SettingMode::Unset),
+                Command::UnsetFullscreen => controller.fullscreen_window(window, SettingMode::Unset),
+                Command::UnsetTiled => controller.tile_window(window, SettingMode::Unset),
+                Command::Menu | Command::SendToDesktop | Command::SetStatus
+                    | Command::SwitchDesktop => panic!("unhandled command"),
+            };
+            return result.map_err(|e| e.to_string());
         }
     }
 
-    fn fullscreen_window(display: *mut xlib::Display, window: xlib::Window, mode: u64) -> Result<(), &'static str> {
-        require_ewmh_atom(display, NetWMStateFullscreen)?;
-        let mut data = xlib::ClientMessageData::new();
-        data.set_long(0, mode as i64);
-        data.set_long(1, NetWMStateFullscreen.to_xlib_atom(display) as i64);
-        send_client_message(display, NetWMState, window, data);
-        return Ok(());
-    }
-
-    fn menu(display: *mut xlib::Display, window: xlib::Window, args: Args) -> Result<(), &'static str> {
+    fn menu(controller: &impl WMController<xlib::Window>, window: xlib::Window, args: Args) -> Result<(), String> {
         let command = match display_menu() {
             Ok(cmd) => cmd,
-            Err(e) => { eprintln!("Error: {}", e); return Err("unable to display menu"); },
+            Err(e) => { eprintln!("Error: {}", e); return Err("unable to display menu".to_owned()); },
         };
-        return command.execute(display, window, args);
-    }
-
-    fn pin_window(display: *mut xlib::Display, window: xlib::Window) -> Result<(), &'static str> {
-        require_ewmh_atom(display, NetWMDesktop)?;
-        let mut data = xlib::ClientMessageData::new();
-        data.set_long(0, -1);
-        send_client_message(display, NetWMDesktop, window, data);
-        return Ok(());
-    }
-
-    fn send_window_to_desktop(display: *mut xlib::Display, window: xlib::Window, desktop: Option<usize>)
-            -> Result<(), &'static str> {
-        require_ewmh_atom(display, NetWMDesktop)?;
-        let desktop = match desktop {
-            Some(desktop) => match desktop.try_into() {
-                Ok(desktop) => desktop,
-                Err(_) => return Err("Invalid desktop index"),
-            },
-            None => return Err("Please supply a desktop index"),
-        };
-        let mut data = xlib::ClientMessageData::new();
-        data.set_long(0, desktop);
-        send_client_message(display, NetWMDesktop, window, data);
-        return Ok(());
-    }
-
-    fn set_status(display: *mut xlib::Display, status: Option<String>) -> Result<(), &'static str> {
-        let status = match status {
-            Some(status) => status,
-            None => return Err("Please supply a status string"),
-        };
-
-        unsafe {
-            let root = xlib::XDefaultRootWindow(display);
-            let success = xlib::XChangeProperty(display, root, MarsStatus.to_xlib_atom(display),
-                UTF8String.to_xlib_atom(display), 8, xlib::PropModeReplace, status.as_ptr(), status.len() as i32);
-            if success == 0 {
-                return Err("Error setting property on root window");
-            } else {
-                xlib::XFlush(display);
-                return Ok(());
-            }
-        }
-    }
-
-    fn simple_window_message(display: *mut xlib::Display, window: xlib::Window, atom: X11Atom)
-            -> Result<(), &'static str> {
-        require_ewmh_atom(display, atom)?;
-        let data = xlib::ClientMessageData::new();
-        send_client_message(display, atom, window, data);
-        return Ok(());
-    }
-
-    fn switch_desktop(display: *mut xlib::Display, desktop: Option<usize>)
-            -> Result<(), &'static str> {
-        require_ewmh_atom(display, NetCurrentDesktop)?;
-        let desktop = match desktop {
-            Some(desktop) => match desktop.try_into() {
-                Ok(desktop) => desktop,
-                Err(_) => return Err("Invalid desktop index"),
-            },
-            None => return Err("Please supply a desktop index"),
-        };
-        let mut data = xlib::ClientMessageData::new();
-        data.set_long(0, desktop);
-        send_client_message(display, NetCurrentDesktop, 0, data);
-        return Ok(());
-    }
-
-    fn tile_window(display: *mut xlib::Display, window: xlib::Window, mode: u64) -> Result<(), &'static str> {
-        require_ewmh_atom(display, MarsWMStateTiled)?;
-        let mut data = xlib::ClientMessageData::new();
-        data.set_long(0, mode as i64);
-        data.set_long(1, MarsWMStateTiled.to_xlib_atom(display) as i64);
-        send_client_message(display, NetWMState, window, data);
-        return Ok(());
-    }
-
-    fn unpin_window(display: *mut xlib::Display, window: xlib::Window) -> Result<(), &'static str> {
-        require_ewmh_atom(display, NetWMDesktop)?;
-        require_ewmh_atom(display, NetCurrentDesktop)?;
-        let root = unsafe { xlib::XDefaultRootWindow(display) };
-        let data = root.x11_read_property_long(display, NetCurrentDesktop, xlib::XA_CARDINAL)?;
-        return Self::send_window_to_desktop(display, window, Some(data[0].try_into().unwrap()));
-    }
-}
-
-fn active_window(display: *mut xlib::Display) -> Result<xlib::Window, &'static str> {
-    require_ewmh_atom(display, NetActiveWindow)?;
-
-    let root = unsafe {
-        xlib::XDefaultRootWindow(display)
-    };
-
-    let data = root.x11_read_property_long(display, NetActiveWindow, xlib::XA_WINDOW)?;
-    if data.len() != 1 {
-        return Err("Query for _NET_ACTIVE_WINDOW returned invalid data");
-    } else {
-        return Ok(data[0]);
-    }
-}
-
-fn require_ewmh_atom(display: *mut xlib::Display, atom: X11Atom) -> Result<(), &'static str> {
-    let root = unsafe {
-        xlib::XDefaultRootWindow(display)
-    };
-    let xatom = atom.to_xlib_atom(display);
-    let supported = root.x11_read_property_long(display, NetSupported, xlib::XA_ATOM)?;
-
-    if supported.contains(&xatom) {
-        return Ok(());
-    } else {
-        eprintln!("Required atom: {} / {}", atom, xatom);
-        return Err("Required atom not supported");
+        return command.execute(controller, window, args);
     }
 }
 
 fn main() {
     let args = Args::parse();
 
-    let display = unsafe {
-        let display = xlib::XOpenDisplay(ptr::null());
-        if display.is_null() {
-            panic!("XOpenDisplay failed");
-        }
-        display
+    let controller = match X11Controller::new() {
+        Ok(ctrl) => ctrl,
+        Err(e) => {
+            eprintln!("Unable to get active window: {}", e);
+            std::process::exit(1);
+        },
     };
 
     let command = args.command;
     let window = match args.window {
         Some(window) => window,
-        None => match active_window(display) {
+        None => match controller.get_active_window() {
             Ok(window) => window,
-            Err(msg) => {
-                eprintln!("Unable to get active window: {}", msg);
+            Err(e) => {
+                eprintln!("Unable to get active window: {}", e);
                 std::process::exit(1);
             },
         },
     };
-    if let Err(msg) = command.execute(display, window, args) {
+    if let Err(msg) = command.execute(&controller, window, args) {
         eprintln!("Error: {}", msg);
         std::process::exit(1);
     }
