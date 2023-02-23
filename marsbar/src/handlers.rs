0c79701eab586e2d86a4d03a8b2c9d308a22640d
jzbor <jzbor@posteo.net>
2023-02-23 02:12:15 +0100

    Implemeting additional workspace actions for marsbar

--- marsbar/src/handlers.rs
+++ marsbar/src/handlers.rs
@@ -1,7 +1,10 @@
 extern crate x11;
 
+use libmars::common::error::*;
 use libmars::common::x11::atoms::X11Atom::*;
 use libmars::common::x11::send_client_message;
+use libmars::control::WMController;
+use libmars::control::x11::X11Controller;
 use libmars::draw::x11::widget::*;
 use x11::xlib::ClientMessageData;
 use x11::xlib;
@@ -29,6 +32,18 @@ impl WorkspaceEventHandler {
         let root = unsafe { xlib::XDefaultRootWindow(display) };
         return WorkspaceEventHandler { display, root, workspace_idx };
     }
+
+    fn cycle_workspace(controller: &impl WMController<xlib::Window>, inc: i32) -> Result<()> {
+        let nworkspaces = controller.count_workspaces()?;
+        let current_workspace = controller.current_workspace()?;
+        let new_workspace = ((current_workspace + nworkspaces) as i32 + inc) as u32 % nworkspaces;
+        return controller.switch_workspace(new_workspace);
+    }
+
+    fn move_client(controller: &impl WMController<xlib::Window>, workspace: u32) -> Result<()> {
+        let active = controller.get_active_window()?;
+        return controller.send_window_to_workspace(active, workspace);
+    }
 }
 
 
@@ -54,13 +69,20 @@ impl WidgetEventHandler for StatusEventHandler {
 impl WidgetEventHandler for WorkspaceEventHandler {
     fn handle_action_event(&self, event: WidgetEvent, already_handled: bool) -> bool {
         if already_handled { return true; }
-        if let WidgetEvent::ButtonPressed(_) = event {
-            let mut data = ClientMessageData::new();
-            data.set_long(0, self.workspace_idx as i64);
-            send_client_message(self.display, NetCurrentDesktop, self.root, data);
-            return true;
-        } else {
-            return false;
+        if let WidgetEvent::ButtonPressed(button) = event {
+            if let Ok(controller) = X11Controller::new() {
+                let result = match button {
+                    1 => controller.switch_workspace(self.workspace_idx),
+                    2 => Self::move_client(&controller, self.workspace_idx),
+                    3 => controller.switch_workspace(self.workspace_idx),
+                    4 => Self::cycle_workspace(&controller, -1),
+                    5 => Self::cycle_workspace(&controller, 1),
+                    _ => return false,
+                };
+
+                return result.is_ok()
+            }
         }
+        return false;
     }
 }
