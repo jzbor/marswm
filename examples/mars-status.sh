0252bdd41d3a3ced6d5a7def7701e61ba17c3e5d
jzbor <jzbor@posteo.net>
2024-01-13 17:42:25 +0100

    Update examples/mars-status.sh to fix issue with HID device batteries

--- examples/mars-status.sh
+++ examples/mars-status.sh
@@ -3,7 +3,8 @@
 set +o nounset
 
 SEPARATOR='\x1f'
-BATTERY_PATH="$(find /sys/class/power_supply -maxdepth 1 -mindepth 1 | grep -i bat | head -n 1)"
+BATTERY_PATH="$(find /sys/class/power_supply -maxdepth 1 -mindepth 1 | { grep -i bat || true; } \
+	| { grep -vi hid || true; } | head -n 1)"
 
 
 ### HELPERS
