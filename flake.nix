38684a58f11ab07c3caf4bdbd2a274a1d7cab4ee
jzbor <jzbor@posteo.net>
2024-12-19 17:14:39 +0100

    Updating flake inputs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
 {
   description = "marswm window manager";
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     cf.url = "github:jzbor/cornflakes";
-    cf.inputs.nixpkgs.follows = "nixpkgs";
     crane.url = "github:ipetkov/crane";
   };
 
